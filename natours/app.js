117f664a6b818373ff6e561c233c576e64846a1c
Phillip Harrington <philsown@gmail.com>
2021-08-11 19:23:11 -0400

    Adding helmet

--- natours/app.js
+++ natours/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,10 +11,20 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // GLOBAL MIDDLEWARES
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware 👋🏼');
+//   next();
+// });
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same client
 // 100 Requests per hour
 const limiter = rateLimit({
   max: 100,
@@ -22,14 +33,13 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
-// app.use((req, res, next) => {
-//   console.log('Hello from the middleware 👋🏼');
-//   next();
-// });
-
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
